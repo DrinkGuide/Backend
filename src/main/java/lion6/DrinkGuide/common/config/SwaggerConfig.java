2bb07ba8c68d5faa282ef19c124501ac48ccf829
JuseungL <121665437+JuseungL@users.noreply.github.com>
2024-07-20 16:48:37 +0900

    Connected Swagger and Create HealthCheck API - close #5

---
+++ src/main/java/lion6/DrinkGuide/common/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package lion6.DrinkGuide.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("DrinkGuide API Specification")
+                .description("API specification for the \"DrinkGuide\" Service.\n\n" +
+                        "Health Check - [http://localhost:8080/api/v1/health](http://localhost:8080/api/v1/health)")
+                .version("1.0.0");
+    }
+}
---
+++ src/main/java/lion6/DrinkGuide/common/exception/NotFoundException.java
@@ -0,0 +1,12 @@
+package lion6.DrinkGuide.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class NotFoundException extends BaseException{
+    public NotFoundException() {
+        super(HttpStatus.NOT_FOUND);
+    }
+    public NotFoundException(String message) {
+        super(HttpStatus.NOT_FOUND, message);
+    }
+}

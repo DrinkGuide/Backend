e0f547c44afcff348086f550d3474b65a294ca6b
JuseungL <121665437+JuseungL@users.noreply.github.com>
2024-07-29 00:53:54 +0900

    [BUG] : 문의등록 서비스 로직 추가 - close #19

--- src/main/java/lion6/DrinkGuide/api/contact/service/ContactCommandService.java
+++ src/main/java/lion6/DrinkGuide/api/contact/service/ContactCommandService.java
@@ -1,6 +1,8 @@
 package lion6.DrinkGuide.api.contact.service;
 
+import lion6.DrinkGuide.api.contact.domain.Contact;
 import lion6.DrinkGuide.api.contact.repository.ContactRepository;
+import lion6.DrinkGuide.api.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,9 +11,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class ContactCommandService {
+    private final MemberRepository memberRepository;
     private final ContactRepository contactRepository;
 
     public void createContact(Long memberId, String title, String content) {
-
+        Contact contact = Contact.builder()
+                .title(title)
+                .content(content)
+                .member(memberRepository.findMemberByIdOrThrow(memberId))
+                .build();
+        contactRepository.save(contact);
     }
 }
